af046b936f551cfca0be339d27dac45991893952
NeginAryapour <negin_aryapour@hotmail.com>
2021-12-19 16:24:57 +0330

    update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
-app = Flask(__name__)
-CORS(app)
+# app = Flask(__name__)
+# CORS(app)
 
 from main import main
 @app.route("/", methods=['POST', 'GET'])
@@ -12,5 +12,11 @@ def home():
     return result
 
 
-if __name__ == '__main__':
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
     app.run()
+
+    
+
+    
