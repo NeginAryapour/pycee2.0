886aa90f014f4d662d26425ca47ed2e71eca5c36
NeginAryapour <negin_aryapour@hotmail.com>
2021-12-19 22:00:32 +0330

    update content.js

--- main/content.js
+++ main/content.js
@@ -172,7 +172,7 @@ function send_text_to_server(error, code) {
         }
     }
     //var type = "find_solutions";
-    request.open("POST", 'http://127.0.0.1:5000/run', true); //'http://127.0.0.1:5000/' https://neginaryapour.pythonanywhere.com/run
+    request.open("POST", 'https://neginaryapour.pythonanywhere.com/run', true); //'http://127.0.0.1:5000/' https://neginaryapour.pythonanywhere.com/run
     var msg = { error, code };
     var msgjson = JSON.stringify(msg);
     request.setRequestHeader('Content-type', 'application/json;charset=UTF-8');
